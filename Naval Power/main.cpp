a9c62cd237dedfa394d3a647f44962de2fa6a900
barbosa42 <orobosaogbeide.ogbeide@gmail.com>
2020-06-08 08:27:59 -0700

    Initial Commit

---
+++ Naval Power/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Naval Power
+//
+//  Created by Orobosa Ogbeide on 6/8/20.
+//  Copyright © 2020 Orobosa Ogbeide. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
